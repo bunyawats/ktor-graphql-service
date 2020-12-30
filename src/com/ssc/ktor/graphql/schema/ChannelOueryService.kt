1f92faad22ad9d99b44e7ee397f89e1bb587b7fb
Bunyawat Singchai <bunyawat.s@gmail.com>
2020-12-30 17:23:47 +0700

    reformat code again

--- src/com/ssc/ktor/graphql/schema/ChannelOueryService.kt
+++ src/com/ssc/ktor/graphql/schema/ChannelOueryService.kt
@@ -1,7 +1,6 @@
 package com.ssc.ktor.graphql.schema
 
 import com.expediagroup.graphql.annotations.GraphQLDescription
-import com.ssc.ktor.graphql.schema.models.Channel
 import com.ssc.ktor.graphql.service.TvService
 import org.kodein.di.DI
 import org.kodein.di.instance
@@ -12,6 +11,6 @@ class ChannelOueryService(kodein: DI) {
 
     @GraphQLDescription("Return list of channel based on ChannelSearchParameters options")
     @Suppress("unused")
-    suspend fun getChannel(id: Int) = tvService.getChannel(id);
+    suspend fun getChannel(id: Int) = tvService.getChannel(id)
 }
 
--- src/com/ssc/ktor/graphql/schema/models/Channel.kt
+++ src/com/ssc/ktor/graphql/schema/models/Channel.kt
@@ -1,7 +1,6 @@
 package com.ssc.ktor.graphql.schema.models
 
 import com.expediagroup.graphql.annotations.GraphQLDescription
-import com.ssc.ktor.graphql.service.TvService
 
 @GraphQLDescription("Contains Channel Metadata, id, title, archived and rank.")
 data class Channel(
